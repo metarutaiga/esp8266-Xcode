d388eeccd50a6d376b7f0588a0aff8f18292b981
TAiGA <>
2023-01-17 23:24:53 +0800

    Use clang instead of gcc

--- build/buildsdk.sh
+++ build/buildsdk.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export BUILD_PATH=$PWD
 export IDF_PATH=$PWD/../ESP8266_RTOS_SDK
-export PATH=$PWD/esp-clang:$PWD/xtensa-lx106-elf/bin:$PATH
+export PATH=$PWD:$PWD/esp-clang/bin:$PWD/xtensa-lx106-elf/bin:$PATH
 
 if [ ! -d "hello_world" ]; then
   cp -r $IDF_PATH/examples/get-started/hello_world .
