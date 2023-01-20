00e774d8d65d0257b8d0d4a66bcf192ed5f15c30
TAiGA <>
2023-01-20 14:47:04 +0800

    Update

--- esp8266/main.c
+++ esp8266/main.c
@@ -40,26 +40,37 @@ void wifi_init_sta(void)
 {
     tcpip_adapter_init();
 
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
 
-    wifi_config_t wifi_config =
+    wifi_config_t wifi_sta_config =
     {
         .sta =
         {
-            .ssid = "wifi",
-            .password = "password",
+            .ssid = "esp8266",
             .threshold.authmode = WIFI_AUTH_WPA_WPA2_PSK,
         },
     };
+    wifi_config_t wifi_ap_config =
+    {
+        .ap =
+        {
+            .ssid = "esp8266",
+            .ssid_len = 7,
+            .password = "password",
+            .authmode = WIFI_AUTH_WPA_WPA2_PSK,
+            .max_connection = 4,
+            .beacon_interval = 100,
+        },
+    };
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_APSTA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_ap_config));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_sta_config));
     ESP_ERROR_CHECK(esp_wifi_start());
 
     ESP_LOGI(TAG, "wifi_init_sta finished.");
@@ -68,6 +79,8 @@ void wifi_init_sta(void)
 
 void app_main()
 {
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
     printf("Hello world!\n");
 
     /* Print chip information */
@@ -80,6 +93,8 @@ void app_main()
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    ESP_LOGI(TAG, "%d/%d\n", esp_get_minimum_free_heap_size(), esp_get_free_heap_size());
 
     ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     wifi_init_sta();
