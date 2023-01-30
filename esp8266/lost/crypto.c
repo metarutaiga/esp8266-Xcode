0836207f522c862152ea03154a6aa5e3390634ea
TAiGA <>
2023-01-30 18:12:42 +0800

    Use boot function instead of crypto

--- esp8266/lost/crypto.c
+++ esp8266/lost/crypto.c
@@ -1,113 +1,68 @@
-#include "esp8266.h"
-#include <time.h>
+#include "eagle.h"
+#define ESP_PLATFORM
+#include <utils/common.h>
+#include <crypto/aes_i.h>
 
-int os_mktime(int year, int month, int day, int hour, int min, int sec, time_t* t)
+int __wrap_rijndaelKeySetupEnc(u32 rk[], const u8 cipherKey[], int keyBits)
 {
-    if (year < 1970 || month < 1 || month > 12 || day < 1 || day > 31 ||
-        hour < 0 || hour > 23 || min < 0 || min > 59 || sec < 0 ||
-        sec > 60)
-        return -1;
+    int i;
+    u32 temp;
 
-    struct tm tm = {};
-    tm.tm_year = year - 1900;
-    tm.tm_mon = month - 1;
-    tm.tm_mday = day;
-    tm.tm_hour = hour;
-    tm.tm_min = min;
-    tm.tm_sec = sec;
+    rk[0] = GETU32(cipherKey     );
+    rk[1] = GETU32(cipherKey +  4);
+    rk[2] = GETU32(cipherKey +  8);
+    rk[3] = GETU32(cipherKey + 12);
 
-    *t = (time_t) mktime(&tm);
-    return 0;
-}
+    if (keyBits == 128) {
+        for (i = 0; i < 10; i++) {
+            temp  = rk[3];
+            rk[4] = rk[0] ^ TE421(temp) ^ TE432(temp) ^ TE443(temp) ^ TE414(temp) ^ RCON(i);
+            rk[5] = rk[1] ^ rk[4];
+            rk[6] = rk[2] ^ rk[5];
+            rk[7] = rk[3] ^ rk[6];
+            rk += 4;
+        }
+        return 10;
+    }
 
-#define AES_PRIV_SIZE (4 * 4 * 15 + 4)
-#define AES_PRIV_NR_POS (4 * 15)
+    rk[4] = GETU32(cipherKey + 16);
+    rk[5] = GETU32(cipherKey + 20);
 
-void* aes_decrypt_init(const u8* key, size_t len)
-{
-    extern void rijndaelKeySetupDec(u32 rk[], const u8 cipherKey[]);
-    u32* rk;
-    if (len != 16)
-        return NULL;
-    rk = (u32*)os_malloc(AES_PRIV_SIZE);
-    if (rk == NULL)
-        return NULL;
-    rijndaelKeySetupDec(rk, key);
-    rk[AES_PRIV_NR_POS] = 10;
-    return rk;
-}
+    if (keyBits == 192) {
+        for (i = 0; i < 8; i++) {
+            temp  = rk[5];
+            rk[6] = rk[0] ^ TE421(temp) ^ TE432(temp) ^ TE443(temp) ^ TE414(temp) ^ RCON(i);
+            rk[7] = rk[1] ^ rk[6];
+            rk[8] = rk[2] ^ rk[7];
+            rk[9] = rk[3] ^ rk[8];
+            if (i == 7)
+                return 12;
+            rk[10] = rk[4] ^ rk[9];
+            rk[11] = rk[5] ^ rk[10];
+            rk += 6;
+        }
+    }
 
-void aes_decrypt_deinit(void *ctx)
-{
-    os_memset(ctx, 0, AES_PRIV_SIZE);
-    os_free(ctx);
-}
+    rk[6] = GETU32(cipherKey + 24);
+    rk[7] = GETU32(cipherKey + 28);
 
-void* aes_encrypt_init(const u8* key, size_t len)
-{
-    extern void rijndaelKeySetupEnc(u32 rk[], const u8 cipherKey[]);
-    u32* rk;
-    if (len != 16)
-        return NULL;
-    rk = (u32*)os_malloc(AES_PRIV_SIZE);
-    if (rk == NULL)
-        return NULL;
-    rijndaelKeySetupEnc(rk, key);
-    rk[AES_PRIV_NR_POS] = 10;
-    return rk;
-}
+    if (keyBits == 256) {
+        for (i = 0; i < 7; i++) {
+            temp  = rk[7];
+            rk[8] = rk[0] ^ TE421(temp) ^ TE432(temp) ^ TE443(temp) ^ TE414(temp) ^ RCON(i);
+            rk[9] = rk[1] ^ rk[8];
+            rk[10] = rk[2] ^ rk[9];
+            rk[11] = rk[3] ^ rk[10];
+            if (i == 6)
+                return 14;
+            temp  = rk[11];
+            rk[12] = rk[4] ^ TE411(temp) ^ TE422(temp) ^ TE433(temp) ^ TE444(temp);
+            rk[13] = rk[5] ^ rk[12];
+            rk[14] = rk[6] ^ rk[13];
+            rk[15] = rk[7] ^ rk[14];
+            rk += 8;
+        }
+    }
 
-void aes_encrypt_deinit(void *ctx)
-{
-    os_memset(ctx, 0, AES_PRIV_SIZE);
-    os_free(ctx);
-}
-
-int aes_wrap(const u8 *kek, int n, const u8 *plain, u8 *cipher)
-{
-    int aes_wrap5(const u8 *kek, size_t kek_len, int n, const u8 *plain, u8 *cipher);
-    return aes_wrap5(kek, 16, n, plain, cipher);
-}
-
-int aes_unwrap(const u8 *kek, int n, const u8 *cipher, u8 *plain)
-{
-    int aes_unwrap5(const u8 *kek, size_t kek_len, int n, const u8 *cipher, u8 *plain);
-    return aes_unwrap5(kek, 16, n, cipher, plain);
-}
-
-void randombytes(u8* data, u64 size)
-{
-    os_get_random(data, size);
-}
-
-void wpa_printf(int level, const char* fmt, ...)
-{
-    extern int ets_putc(int);
-    extern int ets_vprintf(int(*putc)(int), const char* fmt, va_list ap);
-    va_list args;
-    va_start(args, fmt);
-    ets_vprintf(ets_putc, fmt, args);
-    va_end(args);
-    ets_putc('\r');
-    ets_putc('\n');
-}
-
-void wpa_hexdump(int level, const char* title, const void* buf, size_t len)
-{
-    
-}
-
-void wpa_hexdump_key(int level, const char* title, const void* buf, size_t len)
-{
-    
-}
-
-void wpa_hexdump_ascii(int level, const char* title, const void* buf, size_t len)
-{
-    
-}
-
-void wpa_hexdump_ascii_key(int level, const char* title, const void* buf, size_t len)
-{
-    
+    return -1;
 }
